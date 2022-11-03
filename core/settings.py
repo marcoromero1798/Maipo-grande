21fd39c9c931520deb9c9a6a7f859cab380fe7c4
Yusef030 <yusef.saidt@gmail.com>
2022-11-03 19:47:02 -0300

    modificar settings. Cambié nombre en pc_create

--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Maipo_grande', 
+        'NAME': 'maipogrande', 
         'USER': 'postgres', 
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1', 
